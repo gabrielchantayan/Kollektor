f57666d595d9617d6ec6c0bff8ceb5b22977983d
Gabe Chantayan <garnet638@gmail.com>
2024-08-12 15:54:55 -0400

    Appbar updates

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -5,7 +5,10 @@ export default function AppBar() {
 	return (
 		<div id='appBar'>
 			<div id='appTitle'>
-				<h1>{t('kollektor')}</h1>
+				<h1 onClick={() => window.location.href = '/'}>{t('kollektor')}</h1>
+			</div>
+			<div id="accountButton">
+				<h1>{t('login')}</h1>
 			</div>
 		</div>
 	);
